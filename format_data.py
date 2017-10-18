599d0a42e48fbbb0a9e22bd8acf24b533c8752df
liuvzzvuil <liuvzzvuil@gmail.com>
2017-10-18 17:47:10 +0800

    添加归一处理

--- format_data.py
+++ format_data.py
@@ -12,20 +12,31 @@ def get_spa_labels(path=""):
         try:
             each_line = f.readlines()
             list_data_list = [x.split() for x in each_line]
-            spa_data = list(map(lambda x: [float(y) for y in x[:3]], list_data_list))
+            spa_data = np.array(list(map(lambda x: [float(y) for y in x[:3]], list_data_list)))
             labels = [x[-1] for x in list_data_list]
             # np_labels = np.array(labels)
             # num_labels = np.where(np_labels == 'didntLike', 0, np.where(np_labels == 'smallDoses', 1, 2))
             # 在只有1000行的情况下, 下述方法较快, 或许当数据量增大的时候会是第一个快些
-            num_labels = [0 if x == 'didntLike' else 1 if x == 'smallDoses' else 2 for x in labels]
+            num_labels = np.array([1 if x == 'didntLike' else 2 if x == 'smallDoses' else 3 for x in labels])
             code, spa, labels = 0, spa_data, num_labels
         except:
             error = traceback.format_exc()
             code, spa, labels = 9, error, ""
     return code, spa, labels
 
+def auto_norm(single_col_np):
+    """将数据归一化"""
+    """目前的数据格式为[[1,2,3], [2, 3,4]]"""
+    assert isinstance(single_col_np, np.ndarray), "数据类型必须为numpy.ndarray"
+    min = single_col_np.T.min(1)
+    max = single_col_np.T.max(1)
+    new_col = (single_col_np - min)/(max-min)
+    return new_col, min, (max-min)
+
 if __name__ == '__main__':
     tic = time.time()
-    print(get_spa_labels())
+    code, spa, labels = get_spa_labels()
+    # test = np.array([[1., 2., 3.], [3., 4., 2.], [2., 3., 1.], [4., 3., 2.]])
+    print(auto_norm(spa))
     toc = time.time()
     print(toc-tic)
