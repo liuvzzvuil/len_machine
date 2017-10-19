329cebb98339b35ff91062222bf73525b3477b2c
liuvzzvuil <liuvzzvuil@gmail.com>
2017-10-19 20:11:20 +0800

    测试手写图片分类

--- guess_num_image/main.py
+++ guess_num_image/main.py
@@ -7,6 +7,7 @@ sys.path.append('data')
 import kNN
 import get_data
 from functools import partial
+import numpy as np
 
 def self_test():
     spa, labels = get_data.get_spa_labels()
@@ -15,4 +16,9 @@ def self_test():
 
 
 if __name__ == "__main__":
-    print(kNN.test(*self_test()))
+    # print(kNN.test(*self_test()))
+    spa, labels = get_data.get_spa_labels()
+    test_spa = get_data.data2array('test.txt')
+    # nn = list(np.array(test_spa).reshape(-1, 32))
+    # print(nn)
+    print(kNN.classify0(test_spa, spa, labels, 5))
